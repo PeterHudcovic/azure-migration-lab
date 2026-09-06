99d3369360a068952c1eab359b11b7ec7a5b50dd
PeterHudcovic <124920481+PeterHudcovic@users.noreply.github.com>
2026-09-06 21:58:46 +0200

    Fix Azure Key Vault configuration

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -2,10 +2,10 @@ data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "migration" {
   name                = "kv-migration-lab"
-  location            = azurerm_resource_group.main.location
+  location            = "swedencentral"
   resource_group_name = azurerm_resource_group.main.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
 
-  enable_rbac_authorization = true
+  rbac_authorization_enabled = true
 }
